f1c7f13aa81af2882a15357bb3624d201ba53d3e
Grégory Châtel <chatel.gregory@gmail.com>
2019-02-18 16:38:09 +0100

    Cleaner print.

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,3 +1,4 @@
+import torch
 from evaluate_model import evaluate, print_eval
 
 def train(nvsm, device, optimizer, epochs, train_loader, eval_loader,
@@ -10,9 +11,12 @@ def train(nvsm, device, optimizer, epochs, train_loader, eval_loader,
             pred_proba = nvsm(n_grams, doc_ids)
             loss       = loss_function(nvsm, pred_proba, lamb)
             loss.backward()
+            # torch.nn.utils.clip_grad_norm_(nvsm.parameters(), 0.5)
+            # for p in nvsm.parameters():
+            #     p.data.add_(-1e-3, p.grad.data)
             optimizer.step()
             if i % print_every == 0:
-                print(f'[{epoch}, {i}]: {loss}')
+                print(f'[{epoch}, {i}]: {loss:6.4f}')
                 recall_at_ks = evaluate(
                     nvsm          = nvsm,
                     device        = device,
