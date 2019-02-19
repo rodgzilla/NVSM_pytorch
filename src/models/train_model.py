e6e38d6b7cee20e39bb695c58139a38a1810c190
Grégory Châtel <chatel.gregory@gmail.com>
2019-02-19 11:27:34 +0100

    Removing the gradient norm clipping code. (the problem was torch.log(0)).

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,5 +1,3 @@
-import pdb
-
 import torch
 from evaluate_model import evaluate, print_eval
 
@@ -7,17 +5,13 @@ def train(nvsm, device, optimizer, epochs, train_loader, eval_loader,
           k_values, loss_function, lamb, print_every):
     for epoch in range(epochs):
         for i, (n_grams, doc_ids) in enumerate(train_loader):
-            # pdb.set_trace()
             n_grams    = n_grams.to(device)
             doc_ids    = doc_ids.to(device)
             optimizer.zero_grad()
             pred_proba = nvsm(n_grams, doc_ids)
             loss       = loss_function(nvsm, pred_proba, lamb)
             loss.backward()
-            torch.nn.utils.clip_grad_norm_(nvsm.parameters(), 0.5)
-            for p in nvsm.parameters():
-                p.data.add_(-1e-3, p.grad.data)
-            # optimizer.step()
+            optimizer.step()
             if i % print_every == 0:
                 print(f'[{epoch}, {i}]: {loss:6.4f}')
                 recall_at_ks = evaluate(
