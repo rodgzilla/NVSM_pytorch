f77ebae7dbe73e7c02c36ff0e0dcdeaa9dada3bb
Grégory Châtel <chatel.gregory@gmail.com>
2019-02-20 11:47:05 +0100

    Proper test code.

--- src/models/test_model.py
+++ src/models/test_model.py
@@ -19,7 +19,7 @@ def load_model_and_data(model_path,
         model_folder,
         data_folder
     )
-    docs = docs[:20] # temp
+    # docs = docs[:23] # temp
     doc_names             = [doc['name'] for doc in docs]
     nvsm = NVSM(
         n_doc             = len(doc_names),
@@ -34,8 +34,7 @@ def load_model_and_data(model_path,
 
     return stoi, doc_names, nvsm
 
-def query_loop(nvsm, stoi, device, doc_names):
-    # pdb.set_trace()
+def query_loop(nvsm, stoi, device, doc_names, n):
     document_indices = torch.arange(len(doc_names)).view(1, -1)
     document_indices = document_indices.to(device)
     unk_tok_idx      = stoi['<UNK>']
@@ -45,9 +44,12 @@ def query_loop(nvsm, stoi, device, doc_names):
         query_tok_idx = [stoi.get(tok, unk_tok_idx) for tok in query_tok]
         query_tensor  = torch.tensor(query_tok_idx).view(1,-1).to(device)
         result        = nvsm.representation_similarity(query_tensor, document_indices)
-        print(result)
+        closest_doc_ids = result.detach().argsort(descending = True)[:n]
+        print(query)
+        print('\n'.join(f'\t{doc_names[idx]}' for idx in closest_doc_ids))
+        print()
 
 if __name__ == '__main__':
     device                = torch.device('cuda')
     stoi, doc_names, nvsm = load_model_and_data('../../models/nvsm_30_20_10.pt', device)
-    query_loop(nvsm, stoi, device, doc_names)
+    query_loop(nvsm, stoi, device, doc_names, 5)
