977e3bb4372ad41c2cccb99efcf63d015b8e7fde
Grégory Châtel <chatel.gregory@gmail.com>
2019-02-20 12:44:07 +0100

    Training with progress bars.

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,10 +1,21 @@
+from tqdm import tqdm
+
 import torch
-from evaluate_model import evaluate, print_eval
+from evaluate_model import evaluate, generate_eval
 
 def train(nvsm, device, optimizer, epochs, train_loader, eval_loader,
           k_values, loss_function, lamb, print_every):
-    for epoch in range(epochs):
-        for i, (n_grams, doc_ids) in enumerate(train_loader):
+    for epoch in tqdm(range(epochs),
+                      desc = 'Epochs',
+                      ncols = 70):
+        tqdm_train_loader = tqdm(
+            enumerate(train_loader),
+            desc  = 'Batch',
+            total = len(train_loader),
+            ncols = 70,
+            leave = True
+        )
+        for i, (n_grams, doc_ids) in tqdm_train_loader:
             n_grams    = n_grams.to(device)
             doc_ids    = doc_ids.to(device)
             optimizer.zero_grad()
@@ -13,7 +24,7 @@ def train(nvsm, device, optimizer, epochs, train_loader, eval_loader,
             loss.backward()
             optimizer.step()
             if i % print_every == 0:
-                print(f'[{epoch}, {i}]: {loss:6.4f}')
+                nvsm.eval()
                 recall_at_ks = evaluate(
                     nvsm          = nvsm,
                     device        = device,
@@ -21,4 +32,6 @@ def train(nvsm, device, optimizer, epochs, train_loader, eval_loader,
                     recalls       = k_values,
                     loss_function = loss_function,
                 )
-                print_eval(k_values, recall_at_ks)
+                nvsm.train()
+                model_eval = generate_eval(k_values, recall_at_ks)
+                print(f'  [{epoch}, {i:5d}]: {loss:5.4f} || {model_eval}')
