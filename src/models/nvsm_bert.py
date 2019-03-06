02d00b9b29e575de01aef087670b70c93db0c7a3
Grégory Châtel <chatel.gregory@gmail.com>
2019-03-06 14:00:36 +0100

    Adding the attention mask parameter to the Bert query encoder to allow for various sizes of queries.

--- src/models/nvsm_bert.py
+++ src/models/nvsm_bert.py
@@ -14,12 +14,15 @@ class QueryEncoder(nn.Module):
         hidden_size        = self.bert.config.hidden_size
         self.hidden_to_doc = nn.Linear(hidden_size, dim_doc_emb)
 
-    def forward(self, query):
+    def forward(self, query, attention_mask = None):
+        if attention_mask is None:
+            attention_mask = torch.ones_like(query)
         segments_ids             = torch.zeros_like(query)
         enc_layer, pooled_output = self.bert(
             query,
             segments_ids,
-            output_all_encoded_layers = False
+            output_all_encoded_layers = False,
+            attention_mask = attention_mask
         )
         query_emb                = self.hidden_to_doc(pooled_output)
         # pdb.set_trace()
@@ -35,8 +38,8 @@ class NVSMBERT(NVSM):
         )
         self.batchnorm         = nn.BatchNorm1d(dim_doc_emb)
 
-    def query_embedding(self, query):
-        return self.query_encoder(query)
+    def query_embedding(self, query, attention_mask):
+        return self.query_encoder(query, attention_mask)
 
 def loss_function(nvsm, pred, lamb):
     '''
